a773bbbb01e99c96bfbd7398f14010c1612adb15
alibomba <wojci.bro@gmail.com>
2023-10-17 12:36:32 +0200

    cennik

--- frontend/src/pages/cennik/Cennik.tsx
+++ frontend/src/pages/cennik/Cennik.tsx
@@ -1,12 +1,69 @@
 
+import { Link } from 'react-router-dom';
+import ServiceTile from '../../components/serviceTile/ServiceTile';
 
 import styles from './cennik.module.css';
 
 const Cennik = () => {
     return (
-        <div>
-
-        </div>
+        <>
+            <nav className={styles.nav}>
+                <Link to='/kontakt' className={styles.nav__button}>Kontakt</Link>
+                <Link to='/jak-to-dziala' className={styles.nav__button}>Jak to działa</Link>
+                <Link to='/lokacje' className={styles.nav__button}>Lokacje</Link>
+            </nav>
+            <main className={styles.main}>
+                <ServiceTile
+                    title='Organizacja wesela'
+                    image='organizacja-wesela.webp'
+                    price='8 000 - 20 000'
+                >
+                    Pomoc w zaplanowaniu i zrealizowaniu wymarzonego ślubu, włączając w to wybór lokalizacji, dekoracje, catering, muzyczną oprawę i koordynację wydarzenia.
+                </ServiceTile>
+                <ServiceTile
+                    title='Organizacja wesela'
+                    image='organizacja-wesela.webp'
+                    price='8 000 - 20 000'
+                >
+                    Pomoc w zaplanowaniu i zrealizowaniu wymarzonego ślubu, włączając w to wybór lokalizacji, dekoracje, catering, muzyczną oprawę i koordynację wydarzenia.
+                </ServiceTile>
+                <ServiceTile
+                    title='Organizacja wesela'
+                    image='organizacja-wesela.webp'
+                    price='8 000 - 20 000'
+                >
+                    Pomoc w zaplanowaniu i zrealizowaniu wymarzonego ślubu, włączając w to wybór lokalizacji, dekoracje, catering, muzyczną oprawę i koordynację wydarzenia.
+                </ServiceTile>
+                <ServiceTile
+                    title='Organizacja wesela'
+                    image='organizacja-wesela.webp'
+                    price='8 000 - 20 000'
+                >
+                    Pomoc w zaplanowaniu i zrealizowaniu wymarzonego ślubu, włączając w to wybór lokalizacji, dekoracje, catering, muzyczną oprawę i koordynację wydarzenia.
+                </ServiceTile>
+                <ServiceTile
+                    title='Organizacja wesela'
+                    image='organizacja-wesela.webp'
+                    price='8 000 - 20 000'
+                >
+                    Pomoc w zaplanowaniu i zrealizowaniu wymarzonego ślubu, włączając w to wybór lokalizacji, dekoracje, catering, muzyczną oprawę i koordynację wydarzenia.
+                </ServiceTile>
+                <ServiceTile
+                    title='Organizacja wesela'
+                    image='organizacja-wesela.webp'
+                    price='8 000 - 20 000'
+                >
+                    Pomoc w zaplanowaniu i zrealizowaniu wymarzonego ślubu, włączając w to wybór lokalizacji, dekoracje, catering, muzyczną oprawę i koordynację wydarzenia.
+                </ServiceTile>
+                <ServiceTile
+                    title='Organizacja wesela'
+                    image='organizacja-wesela.webp'
+                    price='8 000 - 20 000'
+                >
+                    Pomoc w zaplanowaniu i zrealizowaniu wymarzonego ślubu, włączając w to wybór lokalizacji, dekoracje, catering, muzyczną oprawę i koordynację wydarzenia.
+                </ServiceTile>
+            </main>
+        </>
     )
 }
 
