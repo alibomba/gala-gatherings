db0e4ab2be4e1d3f76732bd63b36ea044ff3f347
alibomba <wojci.bro@gmail.com>
2023-10-26 21:52:53 +0200

    services listing

--- cms/src/pages/service/Service.tsx
+++ cms/src/pages/service/Service.tsx
@@ -1,13 +1,35 @@
 
+import { BsCardImage } from 'react-icons/bs';
 
-
+import Input from '../../components/input/Input';
 import styles from './service.module.css';
 
 const Service = () => {
     return (
-        <div>
-
-        </div>
+        <form encType='multipart/form-data' className={styles.form}>
+            <Input
+                id='name'
+                ariaLabel='Nazwa'
+                placeholder='Nazwa'
+                type='text'
+                maxLength={50}
+            />
+            <Input
+                id='price'
+                ariaLabel='Cena'
+                placeholder='Cena'
+                type='text'
+                maxLength={20}
+            />
+            <textarea id="description" cols={30} rows={10} className={styles.form__textarea} aria-label='Opis' placeholder='Opis' maxLength={255}></textarea>
+            <input type="file" id='image' style={{ display: 'none' }} />
+            <label htmlFor='image' className={styles.form__image}>
+                <BsCardImage />
+            </label>
+            <div className={styles.form__row}>
+                <input type="checkbox" id="isFeatured" />
+            </div>
+        </form>
     )
 }
 
--- cms/src/pages/services/Services.tsx
+++ cms/src/pages/services/Services.tsx
@@ -10,18 +10,65 @@ import ServiceTile from '../../components/serviceTile/ServiceTile';
 import styles from './services.module.css';
 
 const Services = () => {
+    const [services, setServices] = useState<Service[]>([]);
+    const [noResults, setNoResults] = useState<boolean>(false);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
 
+    useEffect(() => {
+        const source = axios.CancelToken.source();
+
+        axiosClient({
+            method: 'get',
+            url: '/services',
+            cancelToken: source.token
+        })
+            .then(res => {
+                setServices(res.data);
+            })
+            .catch(err => {
+                if (err?.response?.status === 404) {
+                    setNoResults(true);
+                }
+                else {
+                    setError('Coś poszło nie tak, spróbuj ponownie później...');
+                }
+            })
+            .finally(() => setIsLoading(false));
+
+        return () => {
+            source.cancel();
+        }
+
+    }, []);
+
+    if (isLoading) {
+        return <Loading />
+    }
+
+    if (error) {
+        return <Error>{error}</Error>
+    }
 
     return (
         <main className={styles.main}>
             <Link to='/uslugi/dodaj' className={styles.main__button}>Dodaj</Link>
             <section className={styles.main__services}>
-                <ServiceTile
-                    id='123'
-                    title='Ekskluzywny pakiet slubny'
-                    price='18 000 - 22 000'
-                    image='ekskluzywny-pakiet-slubny.webp'
-                />
+                {
+                    services.length > 0 &&
+                    services.map(service => {
+                        return (<ServiceTile
+                            key={service.id}
+                            id={service.id}
+                            title={service.title}
+                            price={service.price}
+                            image={service.image}
+                        />)
+                    })
+                }
+                {
+                    noResults && <p className={styles.main__noResults}>Brak usług</p>
+                }
             </section>
         </main>
     )
